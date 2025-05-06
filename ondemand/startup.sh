c40a04c15a615c6ab56d3e2def3c2d3b3d790702
Anthony W. Weed <devnoot@gmail.com>
2025-05-05 21:18:11 -0400

    Working build with ood 4

--- ondemand/startup.sh
+++ ondemand/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Start ondemand-dex in the background
+# config.yaml is default 
 runuser -u ondemand-dex -- /usr/sbin/ondemand-dex serve /etc/ood/dex/config.yaml &
+
 # Start httpd in the foreground
 exec /usr/sbin/httpd -D FOREGROUND
